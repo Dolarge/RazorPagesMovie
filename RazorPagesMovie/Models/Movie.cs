13bcb2f3a0774c0643dd79868695458f6c4fcd45
dh.cheon <dohyun722@daum.net>
2022-03-11 15:22:52 +0900

    Update Add Search1

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -7,9 +8,13 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
 
+        [Display(Name ="Release Data")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; } = string.Empty;
+
+        // Entity Framework Core에서 Price를 데이터베이스의 통화에 올바르게 매핑할 수 있게 한다
+        [Column(TypeName ="decimal (18,2)")]
         public decimal Price { get; set; }
     }
 }
--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -22,9 +23,30 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get;set; }
 
+        /// <summary>
+        /// 사용자가 검색 텍스트 상자에 입력하는 텍스트
+        /// BindProperty => 양식 값 및 쿼리 문자열을 속성과 동일한 이름으로 바인딩
+        /// </summary>
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        /// <summary>
+        /// 장르 목록
+        /// Genres를 통해 사용자는 목록에서 장르를 선택할 수 있다
+        /// </summary>
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
